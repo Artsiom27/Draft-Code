de163b668e9a836d9b630098b0d939f2a86be3d5
Artsiom Tsvikevich <temancvik@gmail.com>
2020-12-23 23:05:57 +0300

    second

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         try {
