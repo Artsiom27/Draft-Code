e73bf01de97589d1d45f7f433fac685ba49fff26
Artsiom Tsvikevich <temancvik@gmail.com>
2020-12-28 22:45:57 +0300

    second commit

--- src/main/java/Results.java
+++ src/main/java/Results.java
@@ -1,8 +1,22 @@
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.List;
+import java.util.Map;
+
 public class Results {
+    private List<Student> student;
     private Integer english;
     private Integer maths;
     private Integer russian;
+    private Map<String, Results> results;
 
+    public List<Student> getStudent() {
+        return student;
+    }
+
+    public void setStudent(List<Student> student) {
+        this.student = student;
+    }
 
     public Integer getEnglish() {
         return english;
@@ -28,8 +42,24 @@ public class Results {
         this.russian = russian;
     }
 
+    public Map<String, Results> getResults() {
+        return results;
+    }
 
+    public void setResults(Results results) {
+        this.results = (Map<String, Results>) results;
+    }
+
+    public String toString() {
+        return new ToStringBuilder(this).append("english", english).append("maths", maths)
+                .append("russian", russian).append(results).toString();
+
+
+    }
 }
+
+
+
 
 
 
